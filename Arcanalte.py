3ad12651c52f35667bf83910fe5a668dc91f98ea
Celsia <136956908+CelsiaSolaraStarflare@users.noreply.github.com>
2025-04-03 21:07:59 +0800

    Update Arcanalte.py

--- Arcanalte.py
+++ Arcanalte.py
@@ -5,48 +5,8 @@ from chatbot import *
 from settings import *
 
 
-# Mock database for simplicity
-users_db = {
-    "admin": "password123"
-}
-current_in = []
-# Sign-up function
-def sign_up():
-    st.title("Sign-Up")
-    username = st.text_input("Enter a username")
-    email = st.text_input("Enter an Email(You will not be asked to authenticate")
-    password = st.text_input("Enter a password", type="password")
-    confirm_password = st.text_input("Confirm password", type="password")
-    if st.button("Sign Up"):
-        if username in users_db and users_db[username] == password:
-            login(username, password)
-        if username in users_db:
-            st.error("Username already exists! Please choose another.")
-        elif password != confirm_password:
-            st.error("Passwords do not match!")
-        else:
-            users_db[username] = password
-            st.success("Sign-up successful! Please log in.")
 
-# Login function
-def login(user=None, passwords=None):
-    st.title("Login")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    if user or passwords:
-        username = user
-        password = passwords
-        
-    if st.button("Login"):
-        if username in users_db and users_db[username] == password:
-            st.success(f"Welcome back, {username}!")
-            with open('tmpdb.censored', mode='w') as file:
-                file.write(str(hash(str(username))))
-                current_in.append(username)
-            return True
-        else:
-            st.error("Invalid username or password.")
-    return False
+
 
 # Intro page function
 def intro_page():
@@ -63,20 +23,4 @@ pages = {
 
 # Main app logic
 st.sidebar.title("Navigation")
-with open('tmpdb.censored', mode='r') as file:
-    const = file.read()
-flag = False
-for i in current_in:
-    if hash(i) == const:
-        flag = True
 
-if flag != True:
-    auth = st.sidebar.radio("Authentication", ["Sign Up", "Login", "App"])
-    if auth == "Sign Up":
-        sign_up()
-    elif auth == "Login":
-        if login():
-            selection = st.sidebar.radio("Go to", list(pages.keys()))
-            pages[selection]()
-    else:
-        st.info("Please log in or sign up to access the application.")
