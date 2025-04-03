2576f3c2d58a7f36011a697b94a74ce21896515a
Celsia <136956908+CelsiaSolaraStarflare@users.noreply.github.com>
2025-04-03 12:16:45 +0800

    Update response.py

--- response.py
+++ response.py
@@ -8,11 +8,27 @@ client = OpenAI(
 
 online = True
 search_mode=1
-def openai_api_call(messages):
+def openai_api_call(messages,mode='Normal'):
     print(messages)
-    chat_completion = client.chat.completions.create(
-        messages=messages,
-        model="qwen-turbo",
-    )
+    if mode=='Normal':
+        chat_completion = client.chat.completions.create(
+            messages=messages,
+            model="qwen-turbo",
+        )
+    elif mode=='Reasoning':
+        chat_completion = client.chat.completions.create(
+            messages=messages,
+            model="qwq-32b",
+        )
+    elif mode=='Long Text':
+        chat_completion = client.chat.completions.create(
+            messages=messages,
+            model="qwen-long",
+        )
+    else:
+        chat_completion = client.chat.completions.create(
+            messages=messages,
+            model="qwen-turbo",
+        )
     print(chat_completion)
     return chat_completion.choices[0].message.content
